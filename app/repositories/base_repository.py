75d76cabb34cbaa2e66800898ff5156f9063d753
Felipe Wormmat <felipe.wormmat@gmail.com>
2021-12-02 19:25:23 -0300

    Theme3 50%

---
+++ app/repositories/base_repository.py
@@ -0,0 +1,24 @@
+from app.models.models import Base
+
+class BaseRepository:
+    def __init__(self, session, model):
+        self.session = session
+        self.model = model
+
+    def get_all(self):
+        return self.session.query(self.model).all()
+
+    
+    def create(self, id: int, attributes: dict):
+        self.session.add(model)
+        self.session.commit()
+
+
+    def update(self, id: int, attributes: dict):
+        self.session.query(self.model).filter_by(id = id).update(attributes)
+        self.session.commit()
+
+
+
+    def get_by_id(self, id: int):
+        return self.session.query(self.model).filter_by(id = id).first()
