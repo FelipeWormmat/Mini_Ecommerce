47d8b114b74c2e4c1c5448b1574d41f9c4716b11
Felipe Wormmat <felipe.wormmat@gmail.com>
2021-12-10 15:30:51 -0300

    Updanting

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from app.api.router import router
 from app.models.models import Base
 from app.db.db import engine
+from fastapi_pagination import add_pagination
 
 app = FastAPI()
-
+add_pagination(app)
 app.include_router(router)
