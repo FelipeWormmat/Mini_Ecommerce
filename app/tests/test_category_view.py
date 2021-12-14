ec78c9b9e42d353177dad85fdf6ce24f708bf462
Felipe Wormmat <felipe.wormmat@gmail.com>
2021-12-14 12:05:11 -0300

    tests updating

---
+++ app/tests/test_category_view.py
@@ -0,0 +1,26 @@
+from fastapi.testclient import TestClient
+
+
+def test_category_create(client: TestClient):
+    response = client.post('/categories/', json={
+        'name': 'Categoria 1'
+    })
+
+    assert response.status_code == 201
+    assert response.json()['id'] == 1
+
+
+def test_category_update(client: TestClient):
+    response = client.post('/categories/', json={
+        'name': 'Categoria 1'
+    })
+    assert response.status_code == 201
+
+    response = client.put(
+        '/categories/1', json={'name': 'Categoria alterada'})
+
+    assert response.status_code == 200
+    assert response.json()['name'] == 'Categoria alterada'
+
+    #response = client.get('/categories/1')
+    #assert response.json()['name'] == 'Categoria alterada'
