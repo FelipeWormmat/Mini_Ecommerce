fc721ce20bce2436cee1fb6540c2ff4638a38454
Felipe Wormmat <felipe.wormmat@gmail.com>
2021-12-16 10:52:33 -0300

    implenting new tests

--- app/tests/test_coupon.py
+++ app/tests/test_coupon.py
@@ -1,31 +1,79 @@
 from fastapi.testclient import TestClient
 
-def test_create(client: TestClient, admin_auth_header):
-    response = client.post('/coupons/', headers=admin_auth_header, json={
-        'code':'test',
-        'expire_at':'2022-12-15T25:14',
-        'limit':None,
-        'type':'value',
-        'value':None
-    })
-    assert response.status_code == 201
-    response = client.get('/coupons/1', headers=admin_auth_header)
-    assert response.status_code == 200
-    assert response.json()['code'] == 'test'
-    assert response.json()['expire_at'] == '2022-12-15T12:25:14'
-    assert response.json()['limit'] == None
-    assert response.json()['type'] == 'value'
-    assert response.json()['value'] == None
 
-def test_update(client:TestClient, admin_auth_header):
-    client.post('/coupons/', headers=admin_auth_header, json={
-        'code':'test',
-        'expire_at':'2022-12-12T15:25:14',
-        'limit':None,
-        'type':'value',
-        'value':None
-    })
-    assert client.put('/coupons/1', headers=admin_auth_header, json={
-        'limit':5,
-        'expire_at': '2055-12-15T12:25:14'
-    }).status_code == 200
+def test_coupon_create(client: TestClient, admin_auth_header, coupon_factory):
+    response = client.post('/coupons/',
+                           headers=admin_auth_header,
+                           json={
+                               'code': '1234CODE',
+                               'expire_at': '2021-12-15T23:42:20.154Z',
+                               'limit': 10,
+                               'type': 'value',
+                               'value': 200
+                           })
+    assert response.status_code == 201
+
+    response = client.get(
+        '/coupons/1', headers=admin_auth_header)
+    assert response.json()['code'] == '1234CODE'
+
+    response = client.post('/coupons/',
+                           headers=admin_auth_header,
+                           json={
+                               'code': '1234CODE',
+                               'expire_at': '2021-12-15T23:42:20.154Z',
+                               'limit': 10,
+                               'type': 'value',
+                               'value': 200
+                           })
+    assert response.status_code == 500
+    assert response.json()[
+        'detail'] == "There is already a coupon with the same code"
+
+
+def test_coupon_update(client: TestClient, admin_auth_header):
+    response = client.post('/coupons/',
+                           headers=admin_auth_header,
+                           json={
+                               'code': '1234CODE',
+                               'expire_at': '2021-12-15T23:42:20.154Z',
+                               'limit': 10,
+                               'type': 'value',
+                               'value': 200
+                           })
+    assert response.status_code == 201
+
+    response = client.put('/coupons/1',
+                          headers=admin_auth_header,
+                          json={
+                              'expire_at': '2021-12-15T23:52:09.950000',
+                              'limit': 15
+                          })
+    assert response.status_code == 202
+
+    response = client.get(
+        '/coupons/1', headers=admin_auth_header)
+    assert response.json()['code'] == '1234CODE'
+
+
+def test_coupon_delete(client: TestClient, admin_auth_header):
+    response = client.post('/coupons/',
+                           headers=admin_auth_header,
+                           json={
+                               'code': '1234CODE',
+                               'expire_at': '2021-12-15T23:42:20.154Z',
+                               'limit': 10,
+                               'type': 'value',
+                               'value': 200
+                           })
+    assert response.status_code == 201
+
+    response = client.get(
+        '/coupons/1', headers=admin_auth_header)
+    assert response.json()['code'] == '1234CODE'
+    id = response.json()['id']
+
+    response = client.delete(f'/coupons/{id}',
+                             headers=admin_auth_header,
+                             json={'id': 'pyint'})
+    assert response.status_code == 202
